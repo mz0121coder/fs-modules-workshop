82a144d52bdc716ad916923bdbf62de40b0be89e
Mohammad Zafar <mzcoder7@gmail.com>
2023-05-09 16:25:36 +0100

    task 2b

--- quote.js
+++ quote.js
@@ -1,24 +1,33 @@
-import fs from "node:fs/promises";
-import { v4 as uuidv4 } from "uuid";
+import fs from 'node:fs/promises';
+import { v4 as uuidv4 } from 'uuid';
 
-const fileName = "quotes.json";
+const fileName = 'quotes.json';
+const quotes = [];
 
 export async function addQuote(quoteText) {
+	// generate random quote with unique id
+	const quote = { id: uuidv4(), quoteText: quoteText };
 
+	const data = JSON.parse(await fs.readFile(fileName, 'utf-8'));
+
+	const newQuotes = [...data, quote];
+
+	const quotesJSON = JSON.stringify(newQuotes);
+
+	// write quotes data
+	await fs.writeFile(fileName, quotesJSON, 'utf-8');
+	console.log(quotesJSON);
+	return quote;
 }
+addQuote('Five four three two one');
+// addQuote('TEST2');
 
 export async function getQuotes() {
-
+	// read all quotes
 }
 
-export async function getRandomQuote() {
+export async function getRandomQuote() {}
 
-}
+export async function editQuote(id, quoteText) {}
 
-export async function editQuote(id, quoteText) {
-
-}
-
-export async function deleteQuote(id) {
-
-}
+export async function deleteQuote(id) {}
